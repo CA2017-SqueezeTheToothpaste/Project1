09c62f3165ea1b09686d6b03080040cd05c31f38
WaterFox <home@WaterFoxde-MacBook-Pro.local>
2017-12-07 15:06:15 +0800

    CPU wire ver 0.2

--- project1_src/CPU.v
+++ project1_src/CPU.v
@@ -28,24 +28,28 @@ PC PC(
     .clk_i      (clk_i),
     .rst_i      (rst_i),
     .start_i    (start_i),
-    .pc_i       (),
-    .pc_o       (inst_addr)
+    .pc_i       (MUX2.data_o),
+    
+	.pc_o       (inst_addr)
 );
 
 Instruction_Memory Instruction_Memory(
     .addr_i     (inst_addr), 
-    .instr_o    ()
+    
+	.instr_o    ()
 );
 
 Adder Add_PC(
     .data1_in   (inst_addr),
     .data2_in   (32'd4),
-    .data_o     (Add_pc_o)
+    
+	.data_o     (Add_pc_o)
 );
 
 Adder ADD(
-	.data1_in	(),
-	.data2_in	(),
+	.data1_in	(Shift_left3232.out_o),
+	.data2_in	(IFID_NxtAddr),
+	
 	.data_o		(MUX1.data1_i),
 );
 
@@ -56,20 +60,15 @@ Registers Registers(
     .RDaddr_i   (write_register), 
     .RDdata_i   (write_data),
     .RegWrite_i (reg_write), 
-    .RSdata_o   (Dread1), 
+    
+	.RSdata_o   (Dread1), 
     .RTdata_o   (MUX_ALUSrc.data1_i) 
-);
-
-MUX32 MUX1(
-	.data1_i	(ADD.data_o),
-	.data2_i	(Add_pc_o),
-	.select_i	(Control.Branch_o & Eq.eq_o),
-	.data_o		(Mux1_o)
 );
 
 Control  Control
 (
 	.Op_i		(instructionp[31:26]),
+   
     .ALUSrc_o	(),
 	.ALUOp_o	(),
 	.RegDst_o	(),
@@ -81,7 +80,73 @@ Control  Control
 	.Jump_o		()
 );
 
-MUX_Ctrl MUX_Ctrl(
+Eq Eq
+(
+	.rd1_i		(),
+	.rd2_i		(),
+	
+	.eq_o		()
+);
+
+MUX32 MUX1(
+	.data1_i	(ADD.data_o),
+	.data2_i	(Add_pc_o),
+	.select_i	(Control.Branch_o & Eq.eq_o),
+	
+	.data_o		(Mux1_o)
+);
+
+MUX32 MUX2(
+    .data1_i    (MUX1.data_o),
+    .data2_i    ({{Mux1_o[31:28]},{Shift_left2628.out_o[27:0]}}),
+    .select_i   (Control.Jump_o),
+    
+	.data_o     ()
+);
+
+MUX5 MUX3(
+    .data1_i    (IDEX_Reg.instr20_16_o),
+    .data2_i    (IDEX_Reg.instr15_11_o),
+    .select_i   (IDEX_Reg.regDst_o),
+    
+	.data_o     ()
+);
+
+MUX32 MUX4(
+    .data1_i    (),
+    .data2_i    (IDEX_Reg.signExtendResult_o),
+    .select_i   (ALUSrc),
+    
+	.data_o     (ALUinput2)
+);
+
+MUX32 MUX5(
+    .data1_i    (),
+    .data2_i    (),
+    .select_i   (),
+    
+	.data_o     ()
+);
+
+MUX32_3 MUX6(
+    .data1_i    (IDEX_Reg.reg2Data_o),
+    .data2_i    (),
+	.data3_i	(),
+    .select_i   (),
+    
+	.data_o     ()
+);
+
+MUX32_3 MUX7(
+    .data1_i    (IDEX_Reg.reg2Data_o),
+    .data2_i    (),
+	.data3_i	(),
+    .select_i   (),
+    
+	.data_o     ()
+);
+
+MUX_Ctrl MUX8(
 	.select_i	(HD.mux_control_o),
 	.ALUSrc_i	(Control.ALUSrc_o),
 	.ALUOp_i	(Control.ALUOp_o),
@@ -90,6 +155,7 @@ MUX_Ctrl MUX_Ctrl(
 	.MemRd_i	(Control.MemRd_o),
 	.MemtoReg_i	(Control.MemtoReg_o),
 	.RegWr_i	(Control.RegWr_o),
+	
 	.ALUSrc_o	(),
 	.ALUOp_o	(),
 	.RegDst_o	(),
@@ -98,29 +164,6 @@ MUX_Ctrl MUX_Ctrl(
 	.MemtoReg_o	(),
 	.RegWr_o	()
 );
-
-Eq Eq
-(
-	.rd1_i		(),
-	.rd2_i		(),
-	.eq_o		()
-);
-
-MUX5 MUX_RegDst(
-    .data1_i    (instruction[20:16]),
-    .data2_i    (instruction[15:11]),
-    .select_i   (reg_dst),
-    .data_o     (write_register)
-);
-
-MUX32 MUX_ALUSrc(
-    .data1_i    (),
-    .data2_i    (extended),
-    .select_i   (ALUSrc),
-    .data_o     (ALUinput2)
-);
-
-
 
 Sign_Extend Sign_Extend(
     .data_i     (instruction[15:0]),
@@ -131,6 +174,7 @@ ALU ALU(
     .data1_i    (Dread1),
     .data2_i    (ALUinput2),
     .ALUCtrl_i  (Alu_ctr),
+   
     .data_o     (write_data),
     .Zero_o     ()
 );
@@ -138,6 +182,7 @@ ALU ALU(
 ALU_Control ALU_Control(
     .funct_i    (instruction[5:0]),
     .ALUOp_i    (AluOp),
+   
     .ALUCtrl_o  (Alu_ctr)
 );
 
@@ -147,13 +192,22 @@ IFID_Reg IFID_Reg(
 	.flush_i			(Control.Jump_o | (Control.Branch_o & Eq.eq_o)),
 	.nextInstrAddr_i	(Add_pc_o),
     .instr_i			(Instruction_Memory.instr_o),
+	
 	.nextInstrAddr_o	(IFID_NxtAddr), 
     .instr_o			(instruction)
 );
 
+Shift_left3232 Shift_left3232(
+	.in_i	(extended),
+	.out_o	()
+);
 
+Shift_left2628 Shift_left2628(
+	.in_i	(instruction[25:0]),
+	.out_o	()
+);
 
-IDEX_Reg IDEX_Reg (
+IDEX_Reg IDEX_Reg(
 	.clk_i				(clk_i),
 	.writeBack_i		(),
 	.memtoReg_i			(),
@@ -169,58 +223,79 @@ IDEX_Reg IDEX_Reg (
 	.instr25_21_i		(instruction[25:21]),
 	.instr20_16_i		(instruction[20:16]),
 	.instr15_11_i		(instruction[15:11]),
-	.writeBack_o,
-	.memtoReg_o,
-	.memRead_o,
-	.memWrite_o,
-	.ALUSrc_o,
-	.ALUOp_o,
-	.regDst_o,
-	.nextInstrAddr_o,
-	.reg1Data_o,
-	.reg2Data_o,
+	
+	.writeBack_o		(),
+	.memtoReg_o			(),
+	.memRead_o			(),
+	.memWrite_o			(),
+	.ALUSrc_o			(),
+	.ALUOp_o			(),
+	.regDst_o			(),
+	.nextInstrAddr_o	(),
+	.reg1Data_o			(),
+	.reg2Data_o			(),
 	.signExtendResult_o	(),
-	.instr25_21_o,
-	.instr20_16_o,
-	.instr15_11_oq
+	.instr25_21_o		(),
+	.instr20_16_o		(),
+	.instr15_11_oq		()
 );
 
-Pipe_Reg EX_MEM(
-	.wb_i		(),
-	.m_i		(),
-	.pc_i		(),
-	.rd1_i		(),
-	.rd2_i		(),
-	.inst_i		(),
-	.wb_o		(),
-	.m_o		(),
-	.pc_o		(),
-	.rd1_o		(),
-	.rd2_o		(),
-	.inst_o		()
-	);
+EXMEM_Reg EXMEM_Reg(
+	.clk_i			(clk_i),
+	.writeBack_i	(),
+	.memtoReg_i		(),
+	.memRead_i		(),
+	.memWrite_i		(),
+	.ALUresult_i	(),
+	.memWriteData_i	(),
+	.regDstAddr_i	(),
+	
+	.writeBack_o	(),
+	.memtoReg_o		(),
+	.memRead_o		(),
+	.memWrite_o		(),
+	.ALUresult_o	(),
+	.memWriteData_o	(),
+	.regDstAddr_o	()
+);
 
-Pipe_Reg MEM_WB(
-	.wb_i		(),
-	.pc_i		(),
-	.rd1_i		(),
-	.rd2_i		(),
-	.inst_i		(),
-	.wb_o		(),
-	.pc_o		(),
-	.rd1_o		(),
-	.rd2_o		(),
-	.inst_o		()
-	);
+MEMWB_Reg MEMWB_Reg(
+	.clk_i			(clk_i),
+	.writeBack_i	(),
+	.memtoReg_i		(),
+	.memReadData_i	(),
+	.ALUresult_i	(),
+	.regDstAddr_i	(),
+	
+	.writeBack_o	(),
+	.memtoReg_o		(),
+	.memReadData_o	(),
+	.ALUresult_o	(),
+	.regDstAddr_o	()
+);
 
 HD HD
 (
 	.if_id_reg_i	(),
 	.id_ex_regrt_i	(),
 	.id_ex_memrd_i	(),
+	
 	.mux_control_o	(),
 	.pc_stall_o		(),
 	.stallHold_o	()
+);
+
+FW FW
+(
+	.IdEx_rs_i	(),
+	.IdEx_rt_i	(),
+	.ExMem_rd_i	(),
+	.ExMem_Wb_i	(),
+	.MemWb_rd_i	(),
+	.MemWb_Wb_i	(),
+	
+	.ForwardA_o	(),
+	.ForwardB_o	()
 );
 
 endmodule
