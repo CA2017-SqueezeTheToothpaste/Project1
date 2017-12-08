55ce79c458c0b60a712f0fb86b7ecedd574352ce
b04902039 <b04902039@linux11.csie.ntu.edu.tw>
2017-12-09 01:17:51 +0800

    revise data mem

--- project1_src/Data_Memory.v
+++ project1_src/Data_Memory.v
@@ -16,18 +16,24 @@ output	[31:0]	RdData_o;
 reg	[7:0]	memory	[0:31];
 reg [31:0]	tmp;
 
-wire	[2:0]	addr;
+wire	[4:0]	addr;
 
-assign addr = addr_i[2:0];
+assign addr = addr_i[4:0];
 assign RdData_o = tmp;
 
 always @(*) begin
 	if (MemWr_i == 1) begin
-		memory[addr] = WrData_i;
+		memory[addr] = WrData_i[31:24];
+		memory[addr+5'b00001] = WrData_i[23:16];
+		memory[addr+5'b00010] = WrData_i[15:8];
+		memory[addr+5'b00011] = WrData_i[7:0];
 	end
 	else begin
 		if (MemRd_i == 1) begin
-			tmp = memory[addr];
+			tmp[31:24] = memory[addr];
+			tmp[23:16] = memory[addr+5'b00001];
+			tmp[15:8] = memory[addr+5'b00010];
+			tmp[7:0] = memory[addr+5'b00011];
 		end
 	end
 end
