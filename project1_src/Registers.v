e387baea0c209aec65e5c2dc3154d08cebbe0fb5
B04902037 <b04902037@ntu.edu.tw>
2017-12-08 23:25:00 +0800

    Register delay fix

--- project1_src/Registers.v
+++ project1_src/Registers.v
@@ -17,28 +17,33 @@ input   [4:0]       RTaddr_i;
 input   [4:0]       RDaddr_i;
 input   [31:0]      RDdata_i;
 input               RegWrite_i;
-output	reg	[31:0]      RSdata_o; 
-output	reg	[31:0]      RTdata_o;
+output	[31:0]      RSdata_o; 
+output	[31:0]      RTdata_o;
 
 // Register File
 reg     [31:0]      register        [0:31];
+reg		[31:0]		RSdata;
+reg		[31:0]		RTdata;
+
+assign	RSdata_o = RSdata;
+assign	RTdata_o = RTdata;
 
 // Write then read Data   
 always@(posedge clk_i) begin
 
     if(RegWrite_i)
         register[RDaddr_i] = RDdata_i;
+	#1	
+	RSdata = register[RSaddr_i];
+	RTdata = register[RTaddr_i];
+
+end
+
+/*always@(RSaddr_i or RTaddr_i) begin
 	
 	RSdata_o = register[RSaddr_i];
 	RTdata_o = register[RTaddr_i];
 
-end
-
-always@(RSaddr_i or RTaddr_i) begin
-	
-	RSdata_o = register[RSaddr_i];
-	RTdata_o = register[RTaddr_i];
-
-end
+end*/
   
 endmodule 
