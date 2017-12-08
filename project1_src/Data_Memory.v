b1c02fd6b71d7996cfafccef41fc063b62b3c358
b04902039 <b04902039@linux11.csie.ntu.edu.tw>
2017-12-09 02:33:39 +0800

    idosfoisdvosdipwoqeq

--- project1_src/Data_Memory.v
+++ project1_src/Data_Memory.v
@@ -21,7 +21,22 @@ wire	[4:0]	addr;
 assign addr = addr_i[4:0];
 assign RdData_o = tmp;
 
-always @(*) begin
+wire	[7:0]	m0,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11;
+
+assign m0 = memory[0];
+assign m1 = memory[1];
+assign m2 = memory[2];
+assign m3 = memory[3];
+assign m4 = memory[4];
+assign m5 = memory[5];
+assign m6 = memory[6];
+assign m7 = memory[7];
+assign m8 = memory[8];
+assign m9 = memory[9];
+assign m10 = memory[10];
+assign m11 = memory[11];
+
+always @(MemWr_i | MemRd_i) begin
 	if (MemWr_i == 1) begin
 		memory[addr] = WrData_i[7:0];
 		memory[addr+5'b00001] = WrData_i[15:8];
