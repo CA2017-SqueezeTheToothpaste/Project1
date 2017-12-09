4b7f1f7b19ee2e8dd88d79fe057800e658354163
WaterFox <home@WaterFoxde-MacBook-Pro.local>
2017-12-09 15:05:43 +0800

    removed 1 always

--- project1_src/Data_Memory.v
+++ project1_src/Data_Memory.v
@@ -23,15 +23,6 @@ wire	[4:0]	addr;
 assign addr = addr_i[4:0];
 assign RdData_o = tmp;
 
-always @(addr_i or MemRd_i) begin
-	if (MemRd_i) begin
-		tmp[7:0] = memory[addr];
-		tmp[15:8] = memory[addr+5'b00001];
-		tmp[23:16] = memory[addr+5'b00010];
-		tmp[31:24] = memory[addr+5'b00011];
-	end
-end
-
 always @(posedge clk_i) begin
 	if (MemWr_i) begin
 		memory[addr] = WrData_i[7:0];
@@ -39,7 +30,12 @@ always @(posedge clk_i) begin
 		memory[addr+5'b00010] = WrData_i[23:16];
 		memory[addr+5'b00011] = WrData_i[31:24];
 	end
-	
+	else begin
+		tmp[7:0] = memory[addr];
+		tmp[15:8] = memory[addr+5'b00001];
+		tmp[23:16] = memory[addr+5'b00010];
+		tmp[31:24] = memory[addr+5'b00011];
+	end
 end
 
 
